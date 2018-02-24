9b0c1aa25e000bbe1b1a2f564b0680fa0475e5cb
fengwan <fengwan@staff.sina.com.cn>
2018-02-24 19:11:43 +0800

    finish

---
+++ wasmFooter.js
@@ -0,0 +1,15 @@
+var WebAssemblyModule = function(deps) {
+    var defaultDeps = {
+        'global': { },
+        'env': {
+            'memory': new WebAssembly.Memory({
+                initial: 10,
+                limit: 100}),
+            'table': new WebAssembly.Table({
+                initial: 0,
+                element: 'anyfunc'})
+        }
+    };
+
+    return new WebAssembly.Instance(new WebAssembly.Module(buffer), deps || defaultDeps);
+};
